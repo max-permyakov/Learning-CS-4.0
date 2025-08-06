ec6c2904e25e8ec9333035fe6da9d1b45bc192ab
maksi <max.permyakov100@gmail.com>
2025-08-06 21:23:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Learning_C__4._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
